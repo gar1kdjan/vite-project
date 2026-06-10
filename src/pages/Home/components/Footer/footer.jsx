0af56790476e1ff1484c8aebbda0acb21f18bd1a
Igor <iponomarevch@gmail.com>
2026-06-10 10:50:27 +0300

    first commit

---
+++ src/pages/Home/components/Footer/footer.jsx
@@ -0,0 +1,10 @@
+import { Button } from "../../../components/button/button";
+
+export const footer = () => {
+  return (
+    <footer>
+      <p>footer</p>
+      <Button />
+    </footer>
+  );
+};
